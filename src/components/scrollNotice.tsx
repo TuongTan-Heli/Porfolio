2ccdb32a3810aa11c86e041bc5645b7ecd0a4d83
Tuong Tan <helijackybu@gmail.com>
2025-09-09 21:44:06 +1000

    Remove unused

--- src/components/scrollNotice.tsx
+++ src/components/scrollNotice.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import { motion, AnimatePresence, MotionValue, useMotionValueEvent } from "framer-motion";
 type ScrollNoticeProps = {
     scrollYProgress: MotionValue<number>;
