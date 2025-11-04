a7a367df9c0089e5beb4ac9304a9107d49733976
helijacky <helijackybu@gmail.com>
2025-11-04 12:57:57 +1100

    Fix laggy and draggable footer

--- src/components/LoopMarquee.tsx
+++ src/components/LoopMarquee.tsx
@@ -1,50 +1,51 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { motion, useAnimation } from "framer-motion";
 
 type LoopMarqueeProps = {
-  items: React.ReactNode[];   
-  speedSec?: number;          
-  gapClass?: string;          
-  itemWidthClass?: string;   
+  items: React.ReactNode[];
+  speedSec?: number;
 };
 
 export default function LoopMarquee({
   items,
-  speedSec = 10,
-  gapClass = "gap-6",
-  itemWidthClass = "w-64"     
+  speedSec = 10
 }: LoopMarqueeProps) {
-  const doubled = [...items, ...items];
-
+  const doubled = [...items, ...items, ...items, ...items];
+  const [isDragging, setIsDragging] = useState(false);
   const controls = useAnimation();
 
-  const startLoop = () =>
-    controls.start({
-      x: ["0%", "-50%"],            
-      transition: {
-        duration: speedSec,
-        ease: "linear",
-        repeat: Infinity
-      }
-    });
+  const startLoop = () => {
+    if (!isDragging) {
+      controls.start({
+        x: ["0%", "-50%"],
+        transition: {
+          duration: speedSec,
+          ease: "linear",
+          repeat: Infinity
+        }
+      });
+    }
+  }
+
 
   useEffect(() => {
     startLoop();
-  }, []);
+  }, [isDragging, speedSec, controls]);
 
   return (
     <div
-      className="relative w-full overflow-hidden"
-      onMouseEnter={() => controls.stop()}
-      onMouseLeave={() => startLoop()}
-    >
+      className="relative w-full overflow-hidden cursor-grab active:cursor-grabbing select-none">
       <motion.div
-        className={`flex ${gapClass}`}
+        className={`flex `}
         animate={controls}
-        style={{ width: "max-content" }}
+        style={{ width: "max-content"}}
+        drag="x"
+        dragConstraints={{ left: -5000, right: 5000 }}
+        onDragStart={() => setIsDragging(true)}
+        onDragEnd={() => setIsDragging(false)}
       >
         {doubled.map((node, i) => (
-          <div key={i} className={`shrink-0 ${itemWidthClass}`}>
+          <div key={i} className={`shrink-0 mr-6`}>
             {node}
           </div>
         ))}
--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -44,7 +44,7 @@ export default function Footer() {
     ]
     return (
         <footer className="abosolute bottom-0 pt-4 pb-4 bg-[transparent]">
-            <LoopMarquee items={loopItems} speedSec={10} gapClass="gap-16 md:gap-24 lg:gap-40" itemWidthClass="w-auto flex items-center justify-center" />
+            <LoopMarquee items={loopItems} speedSec={20}/>
         </footer>
     );
 }
